f8003492b69395b8e8bec3a2d22b4f0de829d2f6
tjdus9503 <tjdus03095@gmail.com>
2023-11-14 15:02:25 +0900

    article/write, step 2

--- src/main/java/com/ll/sb20231114/ArticleController.java
+++ src/main/java/com/ll/sb20231114/ArticleController.java
@@ -2,11 +2,21 @@ package com.ll.sb20231114;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ArticleController {
     @GetMapping("/article/write")
     String showWrite() {
         return "article/write";
+    }
+
+    @GetMapping("/article/doWrite")
+    @ResponseBody
+    String doWrite(
+            String title,
+            String body
+    ) {
+        return "게시물이 작성되었습니다.";
     }
 }
