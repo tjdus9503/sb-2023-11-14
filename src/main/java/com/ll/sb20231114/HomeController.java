523f45fb6ab38f74072770120773637e0981cb67
tjdus9503 <tjdus03095@gmail.com>
2023-11-14 11:06:55 +0900

    calc3 ~ 5

--- src/main/java/com/ll/sb20231114/HomeController.java
+++ src/main/java/com/ll/sb20231114/HomeController.java
@@ -2,6 +2,7 @@ package com.ll.sb20231114;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -30,5 +31,32 @@ public class HomeController {
     @ResponseBody
     String showCalc2(Integer a, Integer b) {
         return "a : %d, b : %d".formatted(a, b);
+    }
+
+    @GetMapping("/calc3")
+    @ResponseBody
+    String showCalc3(
+            @RequestParam(defaultValue = "0") int a,
+            @RequestParam(defaultValue = "0") int b
+    ) {
+        return "계산 결과 : %d".formatted(a + b);
+    }
+
+    @GetMapping("/calc4")
+    @ResponseBody
+    String showCalc4(
+            @RequestParam(defaultValue = "0") double a,
+            @RequestParam(defaultValue = "0") double b
+    ) {
+        return "계산 결과 : %f".formatted(a + b);
+    }
+
+    @GetMapping("/calc5")
+    @ResponseBody
+    String showCalc5(
+            @RequestParam(defaultValue = "-") String a,
+            @RequestParam(defaultValue = "-") String b
+    ) {
+        return "계산 결과 : %s".formatted(a + b);
     }
 }
