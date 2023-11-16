29736640223a89670eaa4211d002b056019c6319
tjdus9503 <tjdus03095@gmail.com>
2023-11-16 16:03:26 +0900

    step 54, case

--- src/main/java/com/ll/sb20231114/domain/article/article/controller/ArticleController.java
+++ src/main/java/com/ll/sb20231114/domain/article/article/controller/ArticleController.java
@@ -6,9 +6,11 @@ import com.ll.sb20231114.global.rq.Rq;
 import com.ll.sb20231114.global.rsData.RsData;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,6 +19,7 @@ import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@Validated
 public class ArticleController {
     private final ArticleService articleService;
     private final Rq rq;
@@ -29,17 +32,9 @@ public class ArticleController {
     @PostMapping("/article/write")
     @ResponseBody
     RsData write(
-            String title,
-            String body
+            @NotBlank(message = "제목좀..") String title,
+            @NotBlank String body
     ) {
-        if (title == null || title.trim().length() == 0) {
-            throw new IllegalArgumentException("제목을 입력해주세요.");
-        }
-
-        if (body == null || body.trim().length() == 0) {
-            throw new IllegalArgumentException("내용을 입력해주세요.");
-        }
-
         Article article = articleService.write(title, body);
 
         RsData<Article> rs = new RsData<>(
