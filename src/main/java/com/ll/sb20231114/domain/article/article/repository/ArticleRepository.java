62000269f9ec2d1025d64a0adbd1a0d783286974
tjdus9503 <tjdus03095@gmail.com>
2023-11-16 14:06:06 +0900

    step 42, ArticleArrayList

--- src/main/java/com/ll/sb20231114/domain/article/article/repository/ArticleRepository.java
+++ src/main/java/com/ll/sb20231114/domain/article/article/repository/ArticleRepository.java
@@ -1,14 +1,15 @@
 package com.ll.sb20231114.domain.article.article.repository;
 
 import com.ll.sb20231114.domain.article.article.entity.Article;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class ArticleRepository {
-    private final List<Article> articles = new ArrayList<>();
+    private final ArticleArrayList articles;
 
     public Article save(Article article) {
         if (article.getId() == null) {
