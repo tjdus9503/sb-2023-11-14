604b815989167e8e2bedcf783dd935b58569403f
tjdus9503 <tjdus03095@gmail.com>
2023-11-16 13:08:17 +0900

    step 35, @component 2

--- src/main/java/com/ll/sb20231114/domain/article/article/repository/ArticleRepository.java
+++ src/main/java/com/ll/sb20231114/domain/article/article/repository/ArticleRepository.java
@@ -1,10 +1,12 @@
 package com.ll.sb20231114.domain.article.article.repository;
 
 import com.ll.sb20231114.domain.article.article.entity.Article;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class ArticleRepository {
     private final List<Article> articles = new ArrayList<>();
 
--- src/main/java/com/ll/sb20231114/domain/article/article/service/ArticleService.java
+++ src/main/java/com/ll/sb20231114/domain/article/article/service/ArticleService.java
@@ -2,13 +2,15 @@ package com.ll.sb20231114.domain.article.article.service;
 
 import com.ll.sb20231114.domain.article.article.entity.Article;
 import com.ll.sb20231114.domain.article.article.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component // 저는 단 한번만 생성되고, 그 이후에는 재사용되는 객체입니다.
 public class ArticleService {
-    private final ArticleRepository articleRepository = new ArticleRepository();
+    @Autowired
+    private ArticleRepository articleRepository;
 
     public Article write(String title, String body) {
         Article article = new Article(title, body);
