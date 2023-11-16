c8c7be84e392a3257631293a909983e3299b810d
tjdus9503 <tjdus03095@gmail.com>
2023-11-17 00:03:06 +0900

    step 73

--- src/main/java/com/ll/sb20231114/domain/article/article/controller/ArticleController.java
+++ src/main/java/com/ll/sb20231114/domain/article/article/controller/ArticleController.java
@@ -25,9 +25,10 @@ public class ArticleController {
     private final Rq rq;
 
     @GetMapping("/article/detail/{id}")
-    @ResponseBody
-    String showDetail(@PathVariable long id) {
+    String showDetail(Model model, @PathVariable long id) {
         Article article = articleService.findById(id).get();
+
+        model.addAttribute("article", article);
 
         return "article/detail";
     }
