f8b98563021563815766d3f87fa87235942314a3
tjdus9503 <tjdus03095@gmail.com>
2023-11-16 13:06:37 +0900

    step 35, @component

--- src/main/java/com/ll/sb20231114/domain/article/article/controller/ArticleController.java
+++ src/main/java/com/ll/sb20231114/domain/article/article/controller/ArticleController.java
@@ -3,6 +3,7 @@ package com.ll.sb20231114.domain.article.article.controller;
 import com.ll.sb20231114.domain.article.article.entity.Article;
 import com.ll.sb20231114.domain.article.article.service.ArticleService;
 import com.ll.sb20231114.global.rsData.RsData;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,7 +13,8 @@ import java.util.List;
 
 @Controller
 public class ArticleController {
-    private final ArticleService articleService = new ArticleService();
+    @Autowired // 필드 주입, final 은 뺀다.
+    private ArticleService articleService;
 
     @GetMapping("/article/write")
     String showWrite() {
--- src/main/java/com/ll/sb20231114/domain/article/article/service/ArticleService.java
+++ src/main/java/com/ll/sb20231114/domain/article/article/service/ArticleService.java
@@ -2,9 +2,11 @@ package com.ll.sb20231114.domain.article.article.service;
 
 import com.ll.sb20231114.domain.article.article.entity.Article;
 import com.ll.sb20231114.domain.article.article.repository.ArticleRepository;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component // 저는 단 한번만 생성되고, 그 이후에는 재사용되는 객체입니다.
 public class ArticleService {
     private final ArticleRepository articleRepository = new ArticleRepository();
 
