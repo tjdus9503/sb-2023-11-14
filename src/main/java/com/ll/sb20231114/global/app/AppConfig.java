7c9ad7c96d33fa1f116f86fe522b719baa0fc4de
tjdus9503 <tjdus03095@gmail.com>
2023-11-16 14:11:05 +0900

    step 44, Bean 2

--- src/main/java/com/ll/sb20231114/global/app/AppConfig.java
+++ src/main/java/com/ll/sb20231114/global/app/AppConfig.java
@@ -4,12 +4,19 @@ import com.ll.sb20231114.domain.article.article.entity.Article;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 @Configuration
 public class AppConfig {
     @Bean
+    List<Article> articles2() {
+        return new LinkedList<>();
+    }
+
+    @Bean
     List<Article> articles() {
-        return new java.util.LinkedList<>();
+        return new ArrayList<>();
     }
 }
