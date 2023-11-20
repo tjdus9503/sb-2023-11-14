b46e8e25ad5e8a1a32c52fee5c5b97463447fd58
tjdus9503 <tjdus03095@gmail.com>
2023-11-20 15:25:15 +0900

    step 11, remove cookie

--- src/main/java/com/ll/sb20231114/domain/article/article/controller/ArticleController.java
+++ src/main/java/com/ll/sb20231114/domain/article/article/controller/ArticleController.java
@@ -5,7 +5,6 @@ import com.ll.sb20231114.domain.article.article.entity.Member;
 import com.ll.sb20231114.domain.article.article.service.ArticleService;
 import com.ll.sb20231114.domain.member.member.service.MemberService;
 import com.ll.sb20231114.global.rq.Rq;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
@@ -17,7 +16,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -30,25 +28,10 @@ public class ArticleController {
 
     @GetMapping("/article/list")
     String showList(Model model, HttpServletRequest req) {
-        // 쿠키이름이 loginedMemberId 이것인 것의 값을 가져와서 long 타입으로 변환, 만약에 그런게 없다면, 0을 반환
-        long loginedMemberId = Optional.ofNullable(req.getCookies())
-                .stream()
-                .flatMap(Arrays::stream)
-                .filter(cookie -> cookie.getName().equals("loginedMemberId"))
-                .map(Cookie::getValue)
-                .mapToLong(Long::parseLong)
-                .findFirst()
-                .orElse(0);
-
-        if (loginedMemberId > 0) {
-            Member loginedMember = memberService.findById(loginedMemberId).get();
-            model.addAttribute("loginedMember", loginedMember);
-        }
-
-        long fromSessionLoginedMemberId = 0;
-
-        if (req.getSession().getAttribute("loginedMemberId") != null)
-            fromSessionLoginedMemberId = (long) req.getSession().getAttribute("loginedMemberId");
+        long fromSessionLoginedMemberId = Optional
+                .ofNullable(req.getSession().getAttribute("loginedMemberId"))
+                .map(id -> (long) id)
+                .orElse(0L);
 
         if (fromSessionLoginedMemberId > 0) {
             Member loginedMember = memberService.findById(fromSessionLoginedMemberId).get();
--- src/main/java/com/ll/sb20231114/domain/member/member/controller/MemberController.java
+++ src/main/java/com/ll/sb20231114/domain/member/member/controller/MemberController.java
@@ -3,7 +3,6 @@ package com.ll.sb20231114.domain.member.member.controller;
 import com.ll.sb20231114.domain.article.article.entity.Member;
 import com.ll.sb20231114.domain.member.member.service.MemberService;
 import com.ll.sb20231114.global.rq.Rq;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
@@ -42,10 +41,6 @@ public class MemberController {
         if (!member.getPassword().equals(loginForm.password)) {
             throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
         }
-
-        Cookie cookie = new Cookie("loginedMemberId", member.getId() + "");
-        cookie.setPath("/");
-        response.addCookie(cookie);
 
         HttpSession session = req.getSession();
         session.setAttribute("loginedMemberId", member.getId());
