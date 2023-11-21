e29550816c0f9c1ce44985dab97ab0f76ea3a370
tjdus9503 <tjdus03095@gmail.com>
2023-11-21 10:09:16 +0900

    step 31

--- src/main/java/com/ll/sb20231114/domain/article/article/repository/ArticleRepository.java
+++ src/main/java/com/ll/sb20231114/domain/article/article/repository/ArticleRepository.java
@@ -21,9 +21,9 @@ public class ArticleRepository {
         Member member1 = memberRepository.findById(1L).get();
         Member member2 = memberRepository.findById(2L).get();
 
-        articles.add(new Article(member1, "title1", "content1"));
-        articles.add(new Article(member2, "title2", "content2"));
-        articles.add(new Article(member2, "title3", "content3"));
+        save(new Article(member1, "title1", "content1"));
+        save(new Article(member2, "title2", "content2"));
+        save(new Article(member2, "title3", "content3"));
     }
 
     public Article save(Article article) {
