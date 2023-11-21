976623ff1a4529f79ac8ce1d02bb76f91149c251
tjdus9503 <tjdus03095@gmail.com>
2023-11-21 11:57:55 +0900

    step 39

--- src/main/java/com/ll/sb20231114/global/interceptor/NeedToAdminInterceptor.java
+++ src/main/java/com/ll/sb20231114/global/interceptor/NeedToAdminInterceptor.java
@@ -1,15 +1,26 @@
 package com.ll.sb20231114.global.interceptor;
 
+import com.ll.sb20231114.global.rq.Rq;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
+@RequiredArgsConstructor
 public class NeedToAdminInterceptor implements HandlerInterceptor {
+    private final Rq rq;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        System.out.println("정말로 내가 자동으로 실행되나?");
+        if (!rq.isLogined()) {
+            throw new RuntimeException("로그인 후 이용해주세요.");
+        }
+
+        if (!rq.isAdmin()) {
+            throw new RuntimeException("관리자만 이용할 수 있는 페이지 입니다.");
+        }
 
         return true;
     }
