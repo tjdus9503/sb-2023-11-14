cd7a66e4ef8791af9026517f2f559b63f5c98765
tjdus9503 <tjdus03095@gmail.com>
2023-11-21 10:04:29 +0900

    step 30

--- src/main/java/com/ll/sb20231114/domain/article/article/repository/ArticleRepository.java
+++ src/main/java/com/ll/sb20231114/domain/article/article/repository/ArticleRepository.java
@@ -3,7 +3,6 @@ package com.ll.sb20231114.domain.article.article.repository;
 import com.ll.sb20231114.domain.article.article.entity.Article;
 import com.ll.sb20231114.domain.member.member.entity.Member;
 import com.ll.sb20231114.domain.member.member.repository.MemberRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -11,18 +10,21 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-@RequiredArgsConstructor
 public class ArticleRepository {
     private final MemberRepository memberRepository;
 
-    private final List<Article> articles = new ArrayList<>() {{
+    private final List<Article> articles = new ArrayList<>();
+
+    public ArticleRepository(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+
         Member member1 = memberRepository.findById(1L).get();
         Member member2 = memberRepository.findById(2L).get();
 
-        add(new Article(member1, "title1", "content1"));
-        add(new Article(member2, "title2", "content2"));
-        add(new Article(member2, "title3", "content3"));
-    }};
+        articles.add(new Article(member1, "title1", "content1"));
+        articles.add(new Article(member2, "title2", "content2"));
+        articles.add(new Article(member2, "title3", "content3"));
+    }
 
     public Article save(Article article) {
         if (article.getId() == null) {
