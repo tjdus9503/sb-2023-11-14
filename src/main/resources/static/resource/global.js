e3f7e58d5a5eb4d9b2387ef99c9563e4274d89e5
tjdus9503 <tjdus03095@gmail.com>
2023-11-21 20:55:09 +0900

    step 18

--- src/main/resources/static/resource/global.js
+++ src/main/resources/static/resource/global.js
@@ -34,3 +34,22 @@ function getQueryParams() {
 
     return paramsObj;
 }
+
+$(function () {
+    $('select[value]').each(function (index, el) {
+        const value = $(el).attr('value');
+        if (value) $(el).val(value);
+    });
+
+    $('a[method="post"], a[method="POST"]').click(function (e) {
+        const action = $(this).attr('href');
+        const csfTokenValue = $("meta[name='_csrf']").attr("content");
+
+        // 동적으로 폼을 만든다.
+        const $form = $(`<form action="${action}" method="POST"><input type="hidden" name="_csrf" value="${csfTokenValue}"></form>`);
+        $('body').append($form);
+        $form.submit();
+
+        return false;
+    });
+});
