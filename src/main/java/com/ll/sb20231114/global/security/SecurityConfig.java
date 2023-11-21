2f99b02afa07ac10e44046fcc5fec51f3b80b477
tjdus9503 <tjdus03095@gmail.com>
2023-11-21 14:49:27 +0900

    step 7

--- src/main/java/com/ll/sb20231114/global/security/SecurityConfig.java
+++ src/main/java/com/ll/sb20231114/global/security/SecurityConfig.java
@@ -9,12 +9,6 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(
-                        csrf -> csrf
-                                .disable()
-                );
-
         return http.build();
     }
 }
