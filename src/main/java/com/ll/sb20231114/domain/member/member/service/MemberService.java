9288289aa1a1a22c7d680c969d520be8d2f6f38f
tjdus9503 <tjdus03095@gmail.com>
2023-11-21 15:06:06 +0900

    step 13

--- src/main/java/com/ll/sb20231114/domain/member/member/service/MemberService.java
+++ src/main/java/com/ll/sb20231114/domain/member/member/service/MemberService.java
@@ -3,6 +3,7 @@ package com.ll.sb20231114.domain.member.member.service;
 import com.ll.sb20231114.domain.member.member.entity.Member;
 import com.ll.sb20231114.domain.member.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,8 +14,10 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public Member join(String username, String password) {
+        password = passwordEncoder.encode(password);
         Member member = new Member(username, password);
 
         memberRepository.save(member);
--- src/main/java/com/ll/sb20231114/global/security/SecurityConfig.java
+++ src/main/java/com/ll/sb20231114/global/security/SecurityConfig.java
@@ -3,7 +3,7 @@ package com.ll.sb20231114.global.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -25,6 +25,6 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
