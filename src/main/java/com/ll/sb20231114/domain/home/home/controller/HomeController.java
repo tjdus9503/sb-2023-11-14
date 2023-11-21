3945038ce537c7d24cf02a5acbbd4d34ff8fd63b
tjdus9503 <tjdus03095@gmail.com>
2023-11-21 14:01:39 +0900

    step 3

--- src/main/java/com/ll/sb20231114/domain/home/home/controller/HomeController.java
+++ src/main/java/com/ll/sb20231114/domain/home/home/controller/HomeController.java
@@ -1,12 +1,30 @@
 package com.ll.sb20231114.domain.home.home.controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
     public String goToArticleList() {
         return "redirect:/article/list";
+    }
+
+    @GetMapping("/home/session")
+    @ResponseBody
+    public Map<String, Object> showSession(HttpSession session) {
+        return Collections.list(session.getAttributeNames()).stream()
+                .collect(
+                        Collectors.toMap(
+                                key -> key,
+                                key -> session.getAttribute(key)
+                        )
+                );
     }
 }
