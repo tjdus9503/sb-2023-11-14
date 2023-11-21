77d3a6dfce50d30407d3db465ecb306692b32816
tjdus9503 <tjdus03095@gmail.com>
2023-11-21 11:59:34 +0900

    step 40

--- src/main/java/com/ll/sb20231114/domain/home/home/controller/AdmHomeController.java
+++ src/main/java/com/ll/sb20231114/domain/home/home/controller/AdmHomeController.java
@@ -12,19 +12,11 @@ public class AdmHomeController {
 
     @GetMapping("/adm")
     public String showMain() {
-        if (!rq.isAdmin()) {
-            throw new RuntimeException("관리자만 접근할 수 있습니다.");
-        }
-
         return "home/home/adm/main";
     }
 
     @GetMapping("/adm/home/about")
     public String showAbout() {
-        if (!rq.isAdmin()) {
-            throw new RuntimeException("관리자만 접근할 수 있습니다.");
-        }
-
         return "home/home/adm/about";
     }
 }
